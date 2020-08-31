899eb544478727df55d134a21a75e328be62168e
moshonk <omondi.ken@gmail.com>
2020-08-31 03:21:17 +0300

    Install ruby2.5-dev

--- install_ruby.sh
+++ install_ruby.sh
@@ -6,12 +6,14 @@ echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+sudo apt install -y rbenv
 rbenv install 2.5.0
 rbenv global 2.5.0
 ruby -v
-gem install bundler -v 1.16.6
+sudo gem install bundler -v 1.16.6
 rbenv rehash
 #INSTALL RAILS
-gem install rails -v 5.2.3
+sudo apt-get install ruby2.5-dev -y
+sudo gem install rails -v 5.2.3
 rbenv rehash
 rails -v
